fd3aec2f59b4caad9bcc6163637064701424d6d1
Aline Espino <alin.e.spino@gmail.com>
2017-12-25 16:55:15 -0800

    friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,97 @@
 
-// You should save your application's data inside of app/data/friends.js as an array 
-// of objects. Each of these objects should roughly follow the format below.
 
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
+var friendsArray = [
+
+{
+    name: "Kasper",
+    photo: "http://pure.au.dk/portal/files/81616547/kasperk.jpg",
+    scores:
+    [
+        4,
+        2,
+        1,
+        4,
+        3,
+        2,
+        1,
+        1,
+        5,
+        4
+    ]
+},
+
+{
+    name: "Dimitri",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/7/79/Dimitri_Leonidas_2014.jpg",
+    scores:
+    [
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1
+    ]
+},
+
+{
+    name: "Pascal",
+    photo: "https://i.pinimg.com/736x/6d/7f/3e/6d7f3e46e30edafe09b7cebc5a4e74ff--hawaii-five-o-google-search.jpg",
+    scores:
+    [
+        3,
+        3,
+        4,
+        5,
+        1,
+        3,
+        4,
+        5,
+        4,
+        3
+    ]
+},
+
+{
+    name: "Wolfgang",
+    photo: "http://callingspots.com/wp-content/uploads/2016/11/FB_IMG_1488856733700.jpg",
+    scores:
+    [
+        2,
+        1,
+        4,
+        2,
+        5,
+        3,
+        2,
+        2,
+        4,
+        1
+    ]
+},
+
+{
+    name: "Romeo",
+    photo: "https://img.discogs.com/ug66a30xH7Ol34zR0CAIrK4eyzM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-3094075-1456433371-1072.jpeg.jpg",
+    scores:
+    [
+        5,
+        4,
+        5,
+        3,
+        5,
+        1,
+        3,
+        5,
+        4,
+        2
+    ]
+},
+
+
+
+]
